42d5724e6341e127b9eaa25487d6b756b93e75d2
Pritish <37667762+pritishmehta@users.noreply.github.com>
2024-10-29 20:54:28 +0000

    Deployment_changes

--- dashboard_code/US_market.py
+++ dashboard_code/US_market.py
@@ -277,11 +277,11 @@ with search:
                 st.metric(
                     label=index_name,
                     #for localhost
-                    value=f"${index_data['price']:,.2f}",
-                    delta=f"{index_data['change']:+.2f}%",
+                    #value=f"${index_data['price']:,.2f}",
+                    #delta=f"{index_data['change']:+.2f}%",
                     #for deployment
-                    #value=f"${index_data['price'].iloc[0]:,.2f}",
-                    #delta=f"{index_data['change'].iloc[0]:+.2f}%",
+                    value=f"${index_data['price'].iloc[0]:,.2f}",
+                    delta=f"{index_data['change'].iloc[0]:+.2f}%",
                     delta_color=delta_color
                 )
     else:
