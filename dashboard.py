dbed34d054b5ae834f69884e824890e45fb6b070
Pritish <37667762+pritishmehta@users.noreply.github.com>
2024-10-30 14:15:41 +0000

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -19,6 +19,6 @@ stock_recommendation = st.Page(
     page='dashboard_code/LLM_recommendation.py',
     title='Stock Recommendation'
 )
-pg = st.navigation(pages=[us_market,indian_market])
+pg = st.navigation(pages=[us_market,indian_market,stock_recommendation])
 
 pg.run()
