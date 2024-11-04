83eeb606d523255ce6b36b8b018f3f841dc59a15
Pritish <37667762+pritishmehta@users.noreply.github.com>
2024-11-04 13:52:58 +0000

    Update LLM_recommendation.py

--- dashboard_code/LLM_recommendation.py
+++ dashboard_code/LLM_recommendation.py
@@ -6,5 +6,5 @@ from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, Dropout
 
-data = yf.download('APPL', period = '5y')
+data = yf.download('AAPL', period = '5y')
 st.write(data)
