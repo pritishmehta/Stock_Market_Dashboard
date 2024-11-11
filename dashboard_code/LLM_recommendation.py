a2e3c7202330a2efcb7e3b789b53d90359513827
Pritish <37667762+pritishmehta@users.noreply.github.com>
2024-11-11 09:37:39 +0000

    Update LLM_recommendation.py

--- dashboard_code/LLM_recommendation.py
+++ dashboard_code/LLM_recommendation.py
@@ -8,7 +8,7 @@ from tensorflow.keras.layers import LSTM, Dense, Dropout
 
 # Function to load data
 def load_data(ticker):
-    data = yf.download(ticker, start="2010-01-01", end="2023-01-01")
+    data = yf.download(ticker, period='5y')
     # Reset the index to remove the MultiIndex
     data.reset_index(inplace=True)
     # Assuming 'data' has a MultiIndex, drop the second level of the MultiIndex
