ebddae92ee2c97647e68f4530aa8ced7be490b70
Pritish <37667762+pritishmehta@users.noreply.github.com>
2024-11-13 11:18:45 +0000

    Update LLM_recommendation.py

--- dashboard_code/LLM_recommendation.py
+++ dashboard_code/LLM_recommendation.py
@@ -35,7 +35,8 @@ df["News_Sentiment"] = 0
 for i in range(len(df)):
     news_sentiment = 0
     for article in news["articles"]:
-        if article["publishedAt"] < df.index[i]:
+        article_date = datetime.strptime(article["publishedAt"], "%Y-%m-%dT%H:%M:%SZ")
+        if article_date < df.index[i]:
             news_sentiment += article["sentiment"]
     df.loc[i, "News_Sentiment"] = news_sentiment
 
